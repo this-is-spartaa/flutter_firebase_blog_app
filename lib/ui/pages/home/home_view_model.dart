49b5154d6caf5b6840bddca8f3e1eb3e17fb049d
111coding <jwlee1024@gmail.com>
2024-11-03 03:09:21 +0900

    home view model

---
+++ lib/ui/pages/home/home_view_model.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter_firebase_blog_app/data/model/post.dart';
+import 'package:flutter_firebase_blog_app/data/repository/post_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// 1. 상태 클래스 => List<Post> 로 사용
+// 2. ViewModel 구현
+class HomeViewModel extends Notifier<List<Post>> {
+  HomeViewModel();
+
+  @override
+  List<Post> build() {
+    fetchData();
+    return [];
+  }
+
+  final postRepository = const PostRepository();
+
+  Future<void> fetchData() async {
+    state = await postRepository.getAll();
+  }
+}
+
+// 2. ViewModel 관리자 구현
+final homeViewModel =
+    NotifierProvider<HomeViewModel, List<Post>?>(() => HomeViewModel());
