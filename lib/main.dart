74ede8e3a05904cbd5e92a9e578a4ca056491d21
111coding <jwlee1024@gmail.com>
2024-11-02 21:59:14 +0900

    HomePage appBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          // 앱 전체 색상 purple
+          seedColor: Colors.purple,
+          // 앱 테마 라이트. light 속성은 라이트 테마를 정의하는 곳이니!
+          brightness: Brightness.light,
+        ),
+        // 앱 전체적으로 앱바의 폰트 스타일이 동일하니 appBarTheme 속성 정의해서 사용!
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
       home: HomePage(),
     );
   }
