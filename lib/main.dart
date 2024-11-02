1791928fe8c7e8b20690c026aaec457b06f0561c
111coding <jwlee1024@gmail.com>
2024-11-03 01:29:24 +0900

    firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_blog_app/firebase_options.dart';
 import 'package:flutter_firebase_blog_app/ui/pages/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // ProviderScope 로 앱을 감싸서 RiverPod이 ViewModel 관리할 수 있게 선언
   runApp(const ProviderScope(child: MyApp()));
 }
